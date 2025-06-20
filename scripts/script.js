3587cf141a6c4bec118da231ad53a719b62adab1
Gokay Iseri <gokay.iseri@outlook.com>
2025-06-20 12:36:52 +0300

    2-day
    some icons added
    hero section improved
    about me added
    footer added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
+// Mobile Navigation
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-active");
 });
+
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
